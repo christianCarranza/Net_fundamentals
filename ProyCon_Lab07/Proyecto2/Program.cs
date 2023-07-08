da1f157fd2ed7aa16a9ba621d7ab2522be6523ec
Christian Carranza <x202320691@cibertec.edu.pe>
2023-07-07 20:28:27 -0500

    Sesion 06

---
+++ ProyCon_Lab07/Proyecto2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IntNum1, IntNum2, IntNum3;
+            int IntSuma, IntProm;
+
+            Console.WriteLine("Ingrese el primer numero");
+            IntNum1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero");
+            IntNum2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer numero");
+            IntNum3 = int.Parse(Console.ReadLine());
+
+            IntSuma = IntNum1 + IntNum2 + IntNum3;
+
+            IntProm = IntSuma / 3;
+
+
+            Console.WriteLine("la suma es: " + IntSuma);
+            Console.WriteLine("El promedio es: " + IntProm);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProyCon_Lab07/Proyecto3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double DblPrecio, DblResultado, DblComision;
+            int IntCant;
+
+            Console.WriteLine("Ingrese el precio del producto");
+            DblPrecio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad");
+            IntCant = int.Parse(Console.ReadLine());
+
+            DblResultado = IntCant * DblPrecio;
+            DblComision = DblResultado * 0.15;
+
+            Console.WriteLine("El total es " + DblResultado);
+            Console.WriteLine("La comision del vendedor es " + DblComision);
+            Console.ReadKey();
+
+        }
+    }
+}
